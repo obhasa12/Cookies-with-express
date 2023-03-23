3777bd367b0efb5b4dbc3b99c4cefb84f33d422f
Ninja <cuansenputra@gmail.com>
2023-03-23 09:36:31 +0700

    Setting cookies with express

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisismysecret'));
+
+app.get('/greet', (req, res) => {
+    const { name = 'No-name' } = req.cookies
+    res.send("Hey There! " + name)
+
+});
+
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'henrietta')
+    res.cookie('animal', 'horse')
+    res.send("OK send you a cookies")
+});
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed : true })
+    res.send('sign ok')
+});
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
+});
+
+app.listen(3000, () => {
+    console.log("OPEN IN PORT 3000")
+});
